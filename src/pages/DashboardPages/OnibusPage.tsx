bade71cced63c8ad4cb4f5a6b2f4c9f8c3a3e0e5
Dev_Martinz <Martiinz.742@gmail.com>
2023-07-06 12:12:57 -0300

    Conexao com o Back

--- src/pages/DashboardPages/OnibusPage.tsx
+++ src/pages/DashboardPages/OnibusPage.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { TablesOnibus } from '@app/components/tables/Tables/TablesOnibus';
 import { PageTitle } from '@app/components/common/PageTitle/PageTitle';
+const [onibus, setOnibus] = useState([]);
+
+
+// UseEffect
+useEffect(()=> {
+  fetch('http://localhost:8080/onibus')
+  .then(retorno => retorno.json())
+  .then(retorno_convertido => setOnibus(retorno_convertido));
+}, []);
 
 const OnibusPage: React.FC = () => {
   const { t } = useTranslation();
   return (
     <>
+      <p>{JSON.stringify(onibus)}</p>
       <PageTitle>{t('Onibus')}</PageTitle>
       <TablesOnibus />
     </>
   );
 };
 export default OnibusPage;
+
+
